2c74ef5ba0766500d94e0d814a7880a35c1cf565
N1Chaos <sagiadinos@wanadoo.fr>
2024-06-07 20:35:03 -0400

    message

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,51 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+public_users.get("/",(req,res)=>{
+    res.send(JSON.stringify(books,null,4));
+
+});
+
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: "hihihih"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+    res.send(books[isbn])
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //let booksbyauthor = books.filter({req.params.isbn, req.params.title, req.params.review});
+const author = req.params.author;
+let filtered_books = books.filter((book)=>book.author === author);
+ res.send(filtered_books)
+    //res.send(books[])
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: "mememem"});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const reviews = req.params.reviews;
+    res.send(books[reviews])
 });
 
 module.exports.general = public_users;
